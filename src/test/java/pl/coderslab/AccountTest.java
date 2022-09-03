4f22641ae91b5d6d58a16d90d1bf16c042b4136d
LukaszWiaz <lukasz.wiazowski@gmail.com>
2022-09-03 11:44:46 +0200

    POP

--- src/test/java/pl/coderslab/AccountTest.java
+++ src/test/java/pl/coderslab/AccountTest.java
@@ -1,2 +1,67 @@
-package pl.coderslab;public class AccountTest {
+package pl.coderslab;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pl.coderslab.model.HotelCreateAnAccountPage;
+import pl.coderslab.model.HotelLoginPage;
+import pl.coderslab.model.HotelMainPage;
+import pl.coderslab.model.HotelMyAccountPage;
+
+import java.time.Duration;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
+public class AccountTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp(){
+        //        Ustaw gdzie jest chromedriver -> STEROWNIK
+        System.setProperty("webdriver.chrome.driver",
+                "src/main/resources/drivers/chromedriver.exe");
+        //        Otworz przegladarke
+        this.driver = new ChromeDriver();
+        //        Jesli test nie przechodzi poprawnie, to pewnie za wolno laduje sie strona -> Dodaj czekanie.
+        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void testCreateNewUser() {
+
+        String email = new Random().nextInt(100000000) + "TEA26@test.com";
+        String expectedAlertText = "Your account has been created.";
+
+        // Wejdz na strone glowna
+        this.driver.get("https://hotel-testlab.coderslab.pl/en/");
+
+        HotelMainPage hotelMainPage = new HotelMainPage(this.driver);
+        hotelMainPage.clickSignIn();
+
+        HotelLoginPage hotelLoginPage = new HotelLoginPage(this.driver);
+        hotelLoginPage.createAnAccountWithEmail(email);
+
+        HotelCreateAnAccountPage hotelCreateAnAccountPage = new HotelCreateAnAccountPage(this.driver);
+        hotelCreateAnAccountPage.fillFormAndSubmit("Janusz", "Januszewski", "12345");
+
+        HotelMyAccountPage hotelMyAccountPage = new HotelMyAccountPage(driver);
+        String alertText = hotelMyAccountPage.getAlertText();
+        assertEquals(expectedAlertText, alertText);
+
+        String pageTitle = hotelMyAccountPage.getPageTitle();
+        assertEquals("My account - MyBooking", pageTitle);
+
+    }
+
 }
--- src/test/java/pl/coderslab/ExampleTest.java
+++ src/test/java/pl/coderslab/ExampleTest.java
@@ -1,2 +1,12 @@
-package pl.coderslab;public class ExampleTest {
+package pl.coderslab;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleTest {
+    @Test
+    public void exampleTest(){
+        assertEquals(true, true);
+    }
 }
